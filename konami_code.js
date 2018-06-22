324ac95fb847d32b398f094b5b6f9b6a82d49677
Kelsey Fecho <kfecho30@gmail.com>
2018-06-22 14:01:00 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,5 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
@@ -6,7 +7,10 @@ function init() {
   body.addEventListener('keydown', onKeyDownHandler)
 
   function onKeyDownHandler(e) {
-    
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++
+    }
   }
 }
-
